184e900ee887bc7f33e3d4f3859c5c80806bc2a6
Elena Milan Lopez <emilanlopez@bellarmine.edu>
2019-09-16 03:24:37 -0400

    Updated

--- Application.java
+++ Application.java
@@ -1,5 +1,5 @@
 /**
- *
+ *This class launches the application of the Regexhashmap class.
  *@author Elena Milan Lopez
  *@version 1.0
  *Programming Project One
@@ -18,14 +18,13 @@ public class Application {
 
     public static int lines = 0;
     public static Regexhashmap regex = new Regexhashmap();
-    
-        
+
         public static void main(String[] args){
 
                 File file = new File(args[0]);
-                  
+
                 analyzeFile(file);
-                              
+
                 int secondArg;
 
                 secondArg=Integer.parseInt(args[1]);
@@ -45,9 +44,12 @@ public class Application {
                 System.out.println("There are " + regex.sizeHmUsername() + " unique users in the log.");
 
         }//main
-        
+
         /*
-        
+	This method analize a given file. First reads the file. 
+	Then creates a String of lines and while the line is not a white space it increments the variable lines(which counts the number of lines. 
+	Everytime each line is added to the string, it checks to see if it is an ipv4 or an username.  
+	It also catch FileNotFoundException and IOException.
         */
         public static void analyzeFile(File file){
 
@@ -62,7 +64,7 @@ public class Application {
                                         lines++;
                                         regex.analyzeIpv4(line);
                                         regex.analyzeUsernames(line);
-                                        
+
                                 }//if
                 }//try
                 catch(FileNotFoundException e){
